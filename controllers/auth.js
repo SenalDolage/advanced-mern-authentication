6e82110e4db045a410c0233f1352ca7a31b5bfa2
Senal <senal.dolage@gmail.com>
2022-03-27 23:16:27 +0530

    mongoose added, user model added

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,26 @@
-exports.register = (req, res, next) => {
-    res.send("Register route")
+const User = require('../models/User');
+
+exports.register = async (req, res, next) => {
+    const { username, email, password } = req.body;
+
+    try {
+        const user = await User.create({
+            username,
+            email,
+            password
+        });
+
+        res.status(201).json({
+            sucess: true,
+            user
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            sucess: false,
+            error: error
+        });
+    }
 }
 
 exports.login = (req, res, next) => {
